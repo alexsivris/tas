d55ad569e1a748fb523023ac27bcead0631b5a9d
Alexandros Sivris <alexandros.sivris@gmail.com>
2017-01-15 21:07:49 +0100

    class for visual localization

--- visual_localization/src/visuallocalization.cpp
+++ visual_localization/src/visuallocalization.cpp
@@ -2,5 +2,30 @@
 
 VisualLocalization::VisualLocalization()
 {
+    m_K = new MatrixXf(3,3);
+    m_K->setZero();
+    m_pixelHom.setZero();
+    cout << (*m_K) * m_pixelHom << endl;
+}
 
+void VisualLocalization::broadcastCameraFrame()
+{
+    tf::TransformBroadcaster broadcaster;
+    tf::Transform transf;
+    transf.setOrigin(tf::Vector3(m_camPositionInBaseLink));
+    tf::Quaternion q;
+    q.setRPY(0,0,0); //  MEASUREMENTS
+    transf.setRotation(q);
+    broadcaster.sendTransform(tf::StampedTransform(transf, ros::Time::now(),
+                                                   "/base_link","/camera_frame"));
+
+}
+
+void VisualLocalization::projectPixelToCamera()
+{
+
+}
+
+void VisualLocalization::transformQuaternionToR()
+{
 }
