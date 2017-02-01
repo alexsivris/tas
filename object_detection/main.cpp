9e1dcde2e16165c3b9977c97733e23f30a3a4ef3
Benjamin Braun <benjamin.m.braun@tum.de>
2017-02-01 18:43:17 +0100

    Added launch file. The path to the template images can now be specified in the /launch/landmarkdetector.launch file.

--- object_detection/main.cpp
+++ object_detection/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <ros/ros.h>
 #include "src/landmarkdetector.h"
+
+/**
+ * @brief main Load templates from files and start matching them using the camera image. The paths/filenames to the templates are specified in /launch/landmarkdetector.launch
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char** argv){
-    ros::init(argc, argv, "landmarkdetector_node");
-    ros::NodeHandle nh;
+    ros::init(argc, argv, "landmarkdetector");
+    ros::NodeHandle nh("~");
+
+    string tpl_1, tpl_2, tpl_3;
+    nh.getParam("template_1",tpl_1);
+    nh.getParam("template_2",tpl_2);
+    nh.getParam("template_3",tpl_3);
+
+    vector<string> files = {tpl_1, tpl_2, tpl_3};
 
     LandmarkDetector ld;
-
-    //vector<String> files { "/home/benni/catkin_ws/devel/lib/object_detection/biere.jpg" , "/home/benni/catkin_ws/devel/lib/object_detection/tum.png" , "/home/benni/catkin_ws/devel/lib/object_detection/zaptros.png", "/home/benni/catkin_ws/devel/lib/object_detection/hofbraeuhaus.jpg" };
-
-vector<String> files { "/home/benni/catkin_ws/devel/lib/object_detection/biere.jpg" };
-
-
     ld.computeTemplates(files);
     ld.detectLandmarks(files);
 
